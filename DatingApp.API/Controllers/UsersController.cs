779cfa4716414dce08839199f8cd2ade8df82777
riskless <riskless2k@gmail.com>
2019-07-01 11:20:18 +1200

    End of secion 14

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -25,12 +25,36 @@ namespace DatingApp.API.Controllers
             _repo = repo;
         }
 
+        // [HttpGet]
+        // public async Task<IActionResult> GetUsers()
+        // {
+        //     var users = await _repo.GetUsers();
+
+        //     var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+        //     return Ok(usersToReturn);
+        // }
+
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _repo.GetUsers();
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var userFromRepo = await _repo.GetUser(currentUserId);
+
+            userParams.UserId = currentUserId;
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
+
+            var users = await _repo.GetUsers(userParams);
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            Response.AddPagination(users.CurrentPage, users.PageSize,
+                users.TotalCount, users.TotalPages);
 
             return Ok(usersToReturn);
         }
@@ -45,7 +69,7 @@ namespace DatingApp.API.Controllers
             return Ok(userToReturn);
         }
 
-                [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
         {
             if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
@@ -57,7 +81,7 @@ namespace DatingApp.API.Controllers
 
             if (await _repo.SaveAll())
                 return NoContent();
-            
+
             throw new Exception($"Updating user {id} failed on save");
         }
     }
